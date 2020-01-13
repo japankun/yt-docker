adcbd818b18edf32d98a1ecb48f55a981b4b4c4b
Japan-Kun <scejapankun@yahoo.co.jp>
2020-01-13 20:47:15 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-echo -e "[Privacy]\npassword = $PEERCAST_PASSWORD" > /tmp/peercast-yt/peercast.ini
+echo -e "[Server]\nhtmlPath = html/ja" > /tmp/peercast-yt/peercast.ini
+echo -e "[Privacy]\npassword = $PEERCAST_PASSWORD" >> /tmp/peercast-yt/peercast.ini
 ./peercast
